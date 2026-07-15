3212179865ae35abef8d6e9b9aec3faa59ed88d8
Chirayu Vaishnav <chirayu.college12@gmail.com>
2026-07-15 23:38:51 +0530

    Time: 0 ms (100%), Space: 42 MB (52.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,17 @@
 class Solution {
     public int fib(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return check(n , dp);
+    }
+    public int check(int n , int[] dp){
         if(n==0)
             return 0;
         if(n==1)
             return 1;
-        return fib(n-1)+fib(n-2);
+        if(dp[n] != -1)
+            return dp[n];
+        dp[n] = check(n-1, dp)+check(n-2 , dp);
+        return dp[n];
     }
 }

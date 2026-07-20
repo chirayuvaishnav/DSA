f5fdd795bacee955aa36a8c9dae09d3516e1dcda
Chirayu Vaishnav <chirayu.college12@gmail.com>
2026-07-21 01:17:10 +0530

    Time: 1 ms (25.65%), Space: 43.2 MB (11.27%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0; i<sb.length();i++){
+            char ch=sb.charAt(i);
+            if(ch>=65 &&  ch<=90){
+                sb.setCharAt(i,(char)(ch+32));
+            }
+        }
+        return sb.toString();
+    }
+}

833b9a572f910c4a7f39944cda8f9c664c9f5ddd
Chirayu Vaishnav <chirayu.college12@gmail.com>
2026-07-29 12:14:28 +0530

    Time: 4 ms (99.04%), Space: 48 MB (11.93%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,9 +2,8 @@ class Solution {
     public boolean validPalindrome(String s) {
         int l=0;
         int r=s.length()-1;
-        boolean flag = false;
         while(l<r){
-            if(!flag && s.charAt(l) != s.charAt(r)){
+            if(s.charAt(l) != s.charAt(r)){
                 return isPalindrome(s,l + 1, r) || isPalindrome(s,l, r - 1);
             }
             else{
